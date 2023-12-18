542788ad3b4013f0ae91180876235f6bf34c2f0a
meablelee2013 <meable.lee@gmail.com>
2023-12-18 18:34:41 +0800

    feat: fix compile error

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -2,12 +2,28 @@ import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
+import distributedKVStore from '@ohos.data.distributedKVStore';
 import axios from '@ohos/axios'
 
+let kvManager;
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    let context = this.context;
+    const kvManagerConfig = {
+      context: context,
+      bundleName: 'com.ruma.android'
+    };
+    try {
+      // 创建KVManager实例
+      kvManager = distributedKVStore.createKVManager(kvManagerConfig);
+      console.info('Succeeded in creating KVManager.');
+
+      // 继续创建获取数据库
+    } catch (e) {
+      console.error(`Failed to create KVManager. Code:${e.code},message:${e.message}`);
+    }
   }
 
   onDestroy() {
@@ -19,9 +35,11 @@ export default class EntryAbility extends UIAbility {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
     globalThis.axios = axios.create({
-      baseURL:"https://www.wanandroid.com/",
+      baseURL: "https://www.wanandroid.com/",
       timeout: 1000,
     })
+    globalThis.kvManager = kvManager
+
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
